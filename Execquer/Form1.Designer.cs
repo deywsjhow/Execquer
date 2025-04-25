b43b7faa4a71cbc90c2a7321447e78e899554ced
deywsjhow <jonathan.deyws@aluno.faculdadeimpacta.com.br>
2025-04-25 00:23:42 -0300

    Add project files.

---
+++ Execquer/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿using System.Windows.Forms;
+using System;
+using System.Drawing;
+
+namespace Execquer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify the contents of this method with the code editor.
+        /// </summary>
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Button btnAddFiles;
+        private System.Windows.Forms.Button btnRemoveSelected;
+        private System.Windows.Forms.Button btnMoveUp;
+        private System.Windows.Forms.Button btnMoveDown;
+        private System.Windows.Forms.ListBox lstFiles;
+        private System.Windows.Forms.Label lblPreviewTitle;
+        private System.Windows.Forms.Panel pnlPreviewWrapper;
+        private System.Windows.Forms.TextBox txtPreview;
+        private System.Windows.Forms.Label lblConnection;
+        private System.Windows.Forms.ComboBox cmbConnections;
+        private System.Windows.Forms.Button btnExecute;
+        private System.Windows.Forms.Button btnSave; // Novo botão de salvar
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog; // Caixa de diálogo para salvar
+
+        private void InitializeComponent()
+        {
+            // Update the line causing the error to use the correct constructor for the Icon property.
+            this.Icon = Properties.Resources.exequer_icon; // Original line causing the error
+
+            // Corrected line:
+            this.Icon = new Icon(Properties.Resources.exequer_icon, new Size(32, 32));
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.btnAddFiles = new System.Windows.Forms.Button();
+            this.btnRemoveSelected = new System.Windows.Forms.Button();
+            this.btnMoveUp = new System.Windows.Forms.Button();
+            this.btnMoveDown = new System.Windows.Forms.Button();
+            this.lstFiles = new System.Windows.Forms.ListBox();
+            this.lblPreviewTitle = new System.Windows.Forms.Label();
+            this.pnlPreviewWrapper = new System.Windows.Forms.Panel();
+            this.txtPreview = new System.Windows.Forms.TextBox();
+            this.lblConnection = new System.Windows.Forms.Label();
+            this.cmbConnections = new System.Windows.Forms.ComboBox();
+            this.btnExecute = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button(); // Inicializando o botão de salvar
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog(); // Inicializando o FolderBrowserDialog
+            this.pnlPreviewWrapper.SuspendLayout();
+            this.SuspendLayout();
+
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold);
+            this.lblTitle.Location = new System.Drawing.Point(10, 10);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(720, 30);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Execquer - Executor de Scripts SQL";
+            // 
+            // btnAddFiles
+            // 
+            this.btnAddFiles.Location = new System.Drawing.Point(10, 50);
+            this.btnAddFiles.Name = "btnAddFiles";
+            this.btnAddFiles.Size = new System.Drawing.Size(150, 30);
+            this.btnAddFiles.TabIndex = 1;
+            this.btnAddFiles.Text = "Adicionar arquivos";
+            // 
+            // btnRemoveSelected
+            // 
+            this.btnRemoveSelected.Location = new System.Drawing.Point(170, 50);
+            this.btnRemoveSelected.Name = "btnRemoveSelected";
+            this.btnRemoveSelected.Size = new System.Drawing.Size(160, 30);
+            this.btnRemoveSelected.TabIndex = 2;
+            this.btnRemoveSelected.Text = "Remover selecionado";
+            // 
+            // btnMoveUp
+            // 
+            this.btnMoveUp.Location = new System.Drawing.Point(340, 50);
+            this.btnMoveUp.Name = "btnMoveUp";
+            this.btnMoveUp.Size = new System.Drawing.Size(30, 30);
+            this.btnMoveUp.TabIndex = 3;
+            this.btnMoveUp.Text = "↑";
+            // 
+            // btnMoveDown
+            // 
+            this.btnMoveDown.Location = new System.Drawing.Point(380, 50);
+            this.btnMoveDown.Name = "btnMoveDown";
+            this.btnMoveDown.Size = new System.Drawing.Size(30, 30);
+            this.btnMoveDown.TabIndex = 4;
+            this.btnMoveDown.Text = "↓";
+            // 
+            // lstFiles
+            // 
+            this.lstFiles.Location = new System.Drawing.Point(10, 90);
+            this.lstFiles.Name = "lstFiles";
+            this.lstFiles.Size = new System.Drawing.Size(710, 147);
+            this.lstFiles.TabIndex = 5;
+            // 
+            // lblPreviewTitle
+            // 
+            this.lblPreviewTitle.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.lblPreviewTitle.Location = new System.Drawing.Point(10, 250);
+            this.lblPreviewTitle.Name = "lblPreviewTitle";
+            this.lblPreviewTitle.Size = new System.Drawing.Size(250, 20);
+            this.lblPreviewTitle.TabIndex = 6;
+            this.lblPreviewTitle.Text = "Pré-visualização do Script";
+            // 
+            // pnlPreviewWrapper
+            // 
+            this.pnlPreviewWrapper.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlPreviewWrapper.Controls.Add(this.txtPreview);
+            this.pnlPreviewWrapper.Location = new System.Drawing.Point(10, 275);
+            this.pnlPreviewWrapper.Name = "pnlPreviewWrapper";
+            this.pnlPreviewWrapper.Size = new System.Drawing.Size(710, 200);
+            this.pnlPreviewWrapper.TabIndex = 7;
+            // 
+            // txtPreview
+            // 
+            this.txtPreview.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtPreview.Font = new System.Drawing.Font("Consolas", 10F);
+            this.txtPreview.Location = new System.Drawing.Point(0, 0);
+            this.txtPreview.Multiline = true;
+            this.txtPreview.Name = "txtPreview";
+            this.txtPreview.ReadOnly = true;
+            this.txtPreview.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtPreview.Size = new System.Drawing.Size(708, 198);
+            this.txtPreview.TabIndex = 0;
+            this.txtPreview.WordWrap = false;
+            // 
+            // lblConnection
+            // 
+            this.lblConnection.Location = new System.Drawing.Point(10, 485);
+            this.lblConnection.Name = "lblConnection";
+            this.lblConnection.Size = new System.Drawing.Size(50, 20);
+            this.lblConnection.TabIndex = 8;
+            this.lblConnection.Text = "Conexão: ";
+            // 
+            // cmbConnections
+            // 
+            this.cmbConnections.Location = new System.Drawing.Point(80, 482);
+            this.cmbConnections.Name = "cmbConnections";
+            this.cmbConnections.Size = new System.Drawing.Size(300, 21);
+            this.cmbConnections.TabIndex = 9;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(10, 520);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(150, 30);
+            this.btnSave.TabIndex = 10;
+            this.btnSave.Text = "Salvar Arquivo SQL"; // Novo texto para o botão de salvar
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click); // Adicionando o evento de clique
+            // 
+            // btnExecute
+            // 
+            this.btnExecute.Location = new System.Drawing.Point(400, 480);
+            this.btnExecute.Name = "btnExecute";
+            this.btnExecute.Size = new System.Drawing.Size(150, 30);
+            this.btnExecute.TabIndex = 11;
+            this.btnExecute.Text = "Executar Script";
+            // 
+            // MainForm
+            // 
+            this.ClientSize = new System.Drawing.Size(740, 575); // Novo tamanho do formulário
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.btnAddFiles);
+            this.Controls.Add(this.btnRemoveSelected);
+            this.Controls.Add(this.btnMoveUp);
+            this.Controls.Add(this.btnMoveDown);
+            this.Controls.Add(this.lstFiles);
+            this.Controls.Add(this.lblPreviewTitle);
+            this.Controls.Add(this.pnlPreviewWrapper);
+            this.Controls.Add(this.lblConnection);
+            this.Controls.Add(this.cmbConnections);
+            this.Controls.Add(this.btnExecute);
+            this.Controls.Add(this.btnSave); // Adicionando o botão de salvar
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Execquer";
+            this.pnlPreviewWrapper.ResumeLayout(false);
+            this.pnlPreviewWrapper.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ Execquer/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Windows.Forms;
+
+namespace Execquer
+{
+    public partial class MainForm : Form
+    {
+        private List<string> _sqlFiles = new List<string>();
+        private List<ConnInfo> _availableConnections = new List<ConnInfo>();
+
+        public MainForm()
+        {
+            InitializeComponent();
+            Load += MainForm_Load;
+
+            // Botões
+            btnAddFiles.Click += BtnAddFiles_Click;
+            btnRemoveSelected.Click += BtnRemoveSelected_Click;
+            btnMoveUp.Click += BtnMoveUp_Click;
+            btnMoveDown.Click += BtnMoveDown_Click;
+            btnExecute.Click += BtnExecute_Click;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            _availableConnections = LoadConnectionsFromJson();
+
+            cmbConnections.Items.Clear();
+            cmbConnections.Items.AddRange(_availableConnections.Select(c => c.Name).ToArray());
+
+            if (cmbConnections.Items.Count > 0)
+                cmbConnections.SelectedIndex = 0;
+        }
+
+        private List<ConnInfo> LoadConnectionsFromJson()
+        {
+            string path = "C:\\repos\\Execquer\\Execquer\\Connections.json";
+
+            if (!File.Exists(path))
+                return new List<ConnInfo>();
+
+            var json = File.ReadAllText(path);
+            return JsonSerializer.Deserialize<List<ConnInfo>>(json);
+        }
+
+        private void BtnAddFiles_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog
+            {
+                Multiselect = true,
+                Filter = "SQL Files (*.sql)|*.sql"
+            };
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                foreach (var file in ofd.FileNames)
+                {
+                    if (!_sqlFiles.Contains(file))
+                    {
+                        _sqlFiles.Add(file);
+                        lstFiles.Items.Add(Path.GetFileName(file));
+                    }
+                }
+
+                UpdatePreview();
+            }
+        }
+
+        private void BtnRemoveSelected_Click(object sender, EventArgs e)
+        {
+            int index = lstFiles.SelectedIndex;
+            if (index >= 0 && index < _sqlFiles.Count)
+            {
+                _sqlFiles.RemoveAt(index);
+                lstFiles.Items.RemoveAt(index);
+                UpdatePreview();
+            }
+        }
+
+        private void BtnMoveUp_Click(object sender, EventArgs e)
+        {
+            int index = lstFiles.SelectedIndex;
+            if (index > 0)
+            {
+                Swap(index, index - 1);
+                lstFiles.SelectedIndex = index - 1;
+                UpdatePreview();
+            }
+        }
+
+        private void BtnMoveDown_Click(object sender, EventArgs e)
+        {
+            int index = lstFiles.SelectedIndex;
+            if (index >= 0 && index < _sqlFiles.Count - 1)
+            {
+                Swap(index, index + 1);
+                lstFiles.SelectedIndex = index + 1;
+                UpdatePreview();
+            }
+        }
+
+        private void Swap(int i, int j)
+        {
+            (_sqlFiles[i], _sqlFiles[j]) = (_sqlFiles[j], _sqlFiles[i]);
+
+            var tempItem = lstFiles.Items[i];
+
+            lstFiles.Items[i] = lstFiles.Items[j];
+            lstFiles.Items[j] = tempItem;
+        }
+
+        private void UpdatePreview()
+        {
+            txtPreview.Clear();
+
+            foreach (var file in _sqlFiles)
+            {
+                txtPreview.AppendText($"-- {Path.GetFileName(file)}\r\n");
+                txtPreview.AppendText(File.ReadAllText(file));
+                txtPreview.AppendText("\r\nGO\r\n\r\n");
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            // Abrir diálogo para escolher o diretório
+            DialogResult result = folderBrowserDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                string selectedDirectory = folderBrowserDialog.SelectedPath;
+
+                // Nome do arquivo (pode customizar se quiser perguntar para o usuário)
+                string fileName = $"script_{DateTime.Now:yyyyMMdd_HHmmss}.sql";
+                string fullPath = Path.Combine(selectedDirectory, fileName);
+
+                try
+                {
+                    // Escrever o conteúdo do txtPreview no arquivo
+                    File.WriteAllText(fullPath, txtPreview.Text);
+                    MessageBox.Show($"Arquivo salvo com sucesso em:\n{fullPath}", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Erro ao salvar o arquivo:\n{ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+
+        private void BtnExecute_Click(object sender, EventArgs e)
+        {
+            string connStr = GetSelectedConnectionString();
+
+            if (string.IsNullOrEmpty(connStr))
+            {
+                MessageBox.Show("Selecione uma conexão válida.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtPreview.Text))
+            {
+                MessageBox.Show("Nenhum script para executar.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connStr))
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand(txtPreview.Text, conn);
+                    cmd.ExecuteNonQuery();
+                }
+
+                MessageBox.Show($"Script pronto para execução com a conexão:\n\n{connStr}", "Executar", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro ao executar o script:\n{ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private string GetSelectedConnectionString()
+        {
+            string selectedName = cmbConnections.SelectedItem?.ToString();
+            return _availableConnections.FirstOrDefault(c => c.Name == selectedName)?.ConnectionString;
+        }
+
+        private void lblTitle_Click(object sender, EventArgs e)
+        {
+            // Só pra evitar erro se o evento for ativado por acidente
+        }        
+    }
+}
